7ec3b093e911b32bea61d723deac756876cbc2e2
pablojr17 <pablojr17@gmail.com>
2021-02-27 01:21:00 -0300

    aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,28 @@
-import { CompletedChanllenges } from "../components/CompletedChanllenges";
+import Head from "next/head";
+
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Inicio | move.it</title>
       </Head>
-
       <ExperienceBar />
-
       <section>
         <div>
           <Profile />
-          <CompletedChanllenges />
+          <CompletedChallenges />
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
